4e99a6c95407773060ee9ade90057311bf8a09c7
Lunahri17 <jeremy17games@hotmail.com>
2021-04-08 18:24:43 -0300

    Encaje de Patrones

--- Paradigmas/ProgFuncional/1.PrimerosProgramasHaskell/Hola_Mundo.hs
+++ Paradigmas/ProgFuncional/1.PrimerosProgramasHaskell/Hola_Mundo.hs
@@ -5,3 +5,16 @@ cuadrado x = x * x
 cubo x = x * x * x
 
 hola = "mundo"
+
+--Patron Simple
+fac 0 = 1
+fac x = x * fac (x - 1)
+
+--Patron Tupla
+base (x,y) = x
+altura (x,y) = y
+area (x,y) = x * y
+
+--Patron Lista
+primero (x:xs) = x
+cola (x:xs) = xs
