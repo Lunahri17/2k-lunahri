61d8c5bed73673ae6a50bbca8086c50a60c9ce17
Lunahri17 <jeremy17games@hotmail.com>
2021-04-11 13:38:01 -0300

    Invertir palabra

---
+++ Sintaxis/Problemas_Algoritmicos/Punto4.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <conio.h>
+#include <string.h>
+
+//Protipos de funciones:
+void end();
+
+
+main()
+{
+	char palabra[70],palabra_inv[70];
+    
+    printf("\nIngrese la palabra: ");
+    _flushall();
+    gets(palabra);
+
+    strcpy(palabra_inv,palabra);
+    strrev(palabra_inv);
+    
+    printf("\nLa palabra invertida es: ");
+    puts(palabra_inv);
+    
+	end();
+}
+
+void end()
+{
+	printf("\n\n");
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
