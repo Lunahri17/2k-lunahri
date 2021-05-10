732eb6fde5782e4c1deba8dbbc821207a898c9f6
Lunahri17 <jeremy17games@hotmail.com>
2021-05-10 15:52:15 -0300

    Enunciado apartado d

--- Paradigmas/ProgFuncional/1.TPN1/punto5.hs
+++ Paradigmas/ProgFuncional/1.TPN1/punto5.hs
@@ -1,10 +1,10 @@
 -- Punto 5:
--- a) Fecha a ingresar "20112019" -> Obtener Día, Mes y Año
+-- a) Fecha a ingresar "20112019" -> Obtener Día, Mes y Año:
 dia x = div x 1000000
 mes x = div (mod (x - anio x) 1000000) 10000
 anio x = mod x 10000
 
--- b) Averiguar si un alumno aprobó el parcial (Ap <= 6)
+-- b) Averiguar si un alumno aprobó el parcial (Ap <= 6):
 resultado x 
     | x > 6 = "Promocionado"
     | x == 6 = "Aprobado"
@@ -12,3 +12,5 @@ resultado x
 
 -- c) Promedio de 3 numeros:
 promedio x y z = (x + y + z) / 3
+
+-- d) Volumen de una esfera:
