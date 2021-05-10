4ca30b9e4a24fd99f919e0adb9f68da8e7e5a5a4
Lunahri17 <jeremy17games@hotmail.com>
2021-05-10 15:41:30 -0300

    Comentarios extras

--- Paradigmas/ProgFuncional/1.TPN1/punto5.hs
+++ Paradigmas/ProgFuncional/1.TPN1/punto5.hs
@@ -1,4 +1,5 @@
--- Fecha a ingresar "20112019" -> Obtener Día, Mes y Año
+-- Punto 5:
+-- a) Fecha a ingresar "20112019" -> Obtener Día, Mes y Año
 dia x = div x 1000000
 mes x = div (mod (x - anio x) 1000000) 10000
 anio x = mod x 10000
