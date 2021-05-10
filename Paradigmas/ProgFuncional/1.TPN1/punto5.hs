8b693c4371a80d53702d15410895860db32b4e51
Lunahri17 <jeremy17games@hotmail.com>
2021-05-10 16:26:05 -0300

    Función triangulo

--- Paradigmas/ProgFuncional/1.TPN1/punto5.hs
+++ Paradigmas/ProgFuncional/1.TPN1/punto5.hs
@@ -18,6 +18,9 @@ volumen x =  (4 * pi * x * x * x) / 3
 -- e) Área de un circulo:
 area x = pi * x * x
 
--- f) Ultima cifra de un número entero:
+-- f) Última cifra de un número entero:
 ultimo x = mod x 10
+
+-- g) dado tres números que representan longitudes indicar si pueden formar un triangulo
+triangulo x y z = if (x < (y + z)) && (y < (x + z)) && (z < (x + y)) then "Es triangulo" else "No es triangulo"
 
