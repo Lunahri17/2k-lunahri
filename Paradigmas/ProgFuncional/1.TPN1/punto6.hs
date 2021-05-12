b6d186bc76fd03174331094f5eeeb1812122f762
Lunahri17 <jeremy17games@hotmail.com>
2021-05-12 20:18:14 -0300

    verificaElem

--- Paradigmas/ProgFuncional/1.TPN1/punto6.hs
+++ Paradigmas/ProgFuncional/1.TPN1/punto6.hs
@@ -6,3 +6,8 @@ longCadena (x:xs) = 1 + longCadena xs;
 --- b) Invertir una cadena
 invertirCadena [] = []
 invertirCadena (x:xs) = invertirCadena xs ++ [x]
+
+--- c) verificar si un elemento pertence a la lista
+verificaElem x [] = False
+verificaElem x (y:ys) | x == y = True
+                      | otherwise = verificaElem x ys
