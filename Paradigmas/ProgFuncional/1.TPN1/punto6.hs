28c6ba3ad362b0f229f91827d2d9617be09b7c0b
Lunahri17 <jeremy17games@hotmail.com>
2021-05-12 20:11:41 -0300

    invertirCadena

--- Paradigmas/ProgFuncional/1.TPN1/punto6.hs
+++ Paradigmas/ProgFuncional/1.TPN1/punto6.hs
@@ -2,3 +2,7 @@
 --- a) Longitud de una cadena
 longCadena [] = 0
 longCadena (x:xs) = 1 + longCadena xs;
+
+--- b) Invertir una cadena
+invertirCadena [] = []
+invertirCadena (x:xs) = invertirCadena xs ++ [x]
