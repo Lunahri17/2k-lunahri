d7e03c363e003f0b834cd22c38fa1a4bfd9b0d39
Lunahri17 <jeremy17games@hotmail.com>
2021-05-15 15:33:17 -0300

    Función maxmin

--- Paradigmas/ProgFuncional/1.TPN1/punto8.hs
+++ Paradigmas/ProgFuncional/1.TPN1/punto8.hs
@@ -3,3 +3,7 @@
 sumaComp :: (Int,Int)-> Int
 sumaComp (x,y) = x + y
 
+--- Funciónque recibe 2 enteros y devuelva una tupla donde el primer componente sea el mayor de los dos y la segunda el menor.
+maxmin :: Int -> Int -> (Int, Int)
+maxmin x y | x > y = (x,y)
+           | otherwise = (y,x)
