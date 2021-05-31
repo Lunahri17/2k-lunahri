ac4b607bdeeafa31e6d48c524593bc61f0d6d9ed
Lunahri17 <jeremy17games@hotmail.com>
2021-05-31 17:53:12 -0300

    Me rindo con este programa de mierda

--- Paradigmas/ProgLogica/TPN2/punto4.pl
+++ Paradigmas/ProgLogica/TPN2/punto4.pl
@@ -1,2 +1,2 @@
-repetidos(_, [], 0).
-repetidos(B, [Head|Tail], N) :- repetidos(B, Tail, N1), B == Head, N is N1 + 1.
+repetidos(_, [], _).
+repetidos(Head1, [Head|Tail], N) :- repetidos(Head1, Tail, N1), Head1 == Head, N is N1 + 1.
