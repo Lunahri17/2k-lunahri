6d02c6990c6f79f33c82ef59a19604edc9e2ca0e
Lunahri17 <jeremy17games@hotmail.com>
2021-05-31 18:12:07 -0300

    Punto 8 terminado

--- Paradigmas/ProgLogica/TPN2/punto8.pl
+++ Paradigmas/ProgLogica/TPN2/punto8.pl
@@ -2,3 +2,7 @@ cursa(pedro, paradigmas).
 cursa(mario, paradigmas).
 cursa(pedro, operativos).
 cursa(sara, sintaxis).
+
+materias(Nombre, X) :- findall((Materia), (cursa(Nombre,Materia)),X).
+
+base(X) :- findall((Nombre,Materia), (cursa(Nombre,Materia)),X).
