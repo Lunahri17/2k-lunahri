f8ad09127928732a54e3deafc610c57cdac2f9ba
Lunahri17 <jeremy17games@hotmail.com>
2021-06-01 17:58:10 -0300

    Intento de suma de sueldos

--- Paradigmas/ProgLogica/TPN2/punto10.pl
+++ Paradigmas/ProgLogica/TPN2/punto10.pl
@@ -1,12 +1,18 @@
-empresa([administración,personal,producción]).
+empresa([administracion,personal,produccion]).
 
-empleados(administración,3).
+empleados(administracion,3).
 empleados(personal,1).
-empleados(producción,10).
+empleados(produccion,10).
 
-sueldoPromedio(administración,6000).
+sueldoPromedio(administracion,6000).
 sueldoPromedio(personal,45000).
-sueldoPromedio(producción,77000).
+sueldoPromedio(produccion,77000).
 
 ventas(180000).
 
+totalSueldos(X, Importe) :- findall((NumEmpleados, Puesto, Sueldo), (empleados(Puesto, NumEmpleados), sueldoPromedio(Puesto, Sueldo)), X), sumaSueldos(X, Importe).
+
+sumaSueldos([(Cant, _, Sueld)], Importe).
+sumaSueldos([(Cant, _, Sueld) | Xs], Importe) :- sumaSueldos(Xs, Importe), multi(Cant, Sueld, Z), Importe is Z + Importe.
+
+multi(Cant, Sueld, Z) :- Z is Cant * Sueld.
