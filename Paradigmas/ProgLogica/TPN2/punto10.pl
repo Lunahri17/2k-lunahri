98e38094a90034873dd50ba0182de8053be51901
Lunahri17 <jeremy17games@hotmail.com>
2021-06-01 17:57:51 -0300

    Punto 10, base de conocimiento

---
+++ Paradigmas/ProgLogica/TPN2/punto10.pl
@@ -0,0 +1,12 @@
+empresa([administración,personal,producción]).
+
+empleados(administración,3).
+empleados(personal,1).
+empleados(producción,10).
+
+sueldoPromedio(administración,6000).
+sueldoPromedio(personal,45000).
+sueldoPromedio(producción,77000).
+
+ventas(180000).
+
