d0e1d7694602261fc0bb80718d8552c00d6bccf3
Lunahri17 <jeremy17games@hotmail.com>
2021-06-04 15:34:38 -0300

    idk

--- Paradigmas/ProgOrientadaObjetos/MiPrimeraClase/src/miprimeraclase/Circulo.java
+++ Paradigmas/ProgOrientadaObjetos/MiPrimeraClase/src/miprimeraclase/Circulo.java
@@ -3,6 +3,7 @@ package miprimeraclase;
 public class Circulo {
     double radio = 1;
     public void mostrar(){
+
         System.out.println("Radio: " + radio + ", con un area de: " + calcularArea());
     }
     
