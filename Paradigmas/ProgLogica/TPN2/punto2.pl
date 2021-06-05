5c04f1234f8bc7b566604aaffac45d3c7eb3d7b4
Lunahri17 <jeremy17games@hotmail.com>
2021-06-05 12:29:59 -0300

    Ahora si está joya uwu

--- Paradigmas/ProgLogica/TPN2/punto2.pl
+++ Paradigmas/ProgLogica/TPN2/punto2.pl
@@ -4,41 +4,33 @@ quierea('Pedro', 'Belen').	% Pedro quiere a Belén.
 quierea('Manuel', 'Belen').	% Manuel quiere a Belén.
 quierea('Maria', 'Pedro').	% María quiere a Pedro.
 
-% Todos quieren a Juan y a Maria.
-quierea('Pedro', 'Juan').
-quierea('Maria', 'Juan').
-quierea('Belen', 'Juan').
-quierea('Manuel', 'Juan').
-quierea('Belen', 'Maria').
-quierea('Manuel', 'Maria').
-quierea('Juan', 'Maria').
+quierea('Todos','Maria').   % Todos quieren a María.
+quierea('Todos','Juan').    % Todos quieren a Juan.
 
 % Todo el mundo se quiere a si mismo.
-quierea('Juan', 'Juan').
-quierea('Pedro', 'Pedro').
-quierea('Maria', 'Maria').
-quierea('Belen', 'Belen').
-quierea('Manuel', 'Manuel').
-sequiere(X) :- Y = X, quierea(X, Y).
+quierea('Juan', 'Asimismo').
+quierea('Pedro', 'Asimismo').
+quierea('Maria', 'Asimismo').
+quierea('Belen', 'Asimismo').
+quierea('Manuel', 'Asimismo').
 
-% Alguien quiere a: 
-alguienquierea(X, Y, Z) :- quierea(X, Y), quierea(X, Z).
+quierea('Alguien','Juan').  % Alguien quiere a Juan.
+quierea('Alguien','Pedro'). % Alguien quiere a Pedro.
+quierea('Alguien','Maria'). % Alguien quiere a María.
 
-alguienquiere(A) :- quierea(B,A), B \== A . % Alguien quiere a Maria.
+quierea('Alguien','Todos'). % Alguien quiere a Todos.
 
-% Alguien quiere a todos los demás.
-alguienquieretodos(L) :- A = 'Maria', B = 'Juan', C = 'Pedro', D = 'Belen', E = 'Manuel', quierea(L, A), quierea(L, B), quierea(L, C), quierea(L, D), quierea(L, E).
 
 % Predicados a responder:
-% ¿Quiere Manuel a María? 			            -> quiera('Manuel', 'Maria').
+% ¿Quiere Manuel a María? 			            -> quierea('Manuel', 'Maria').
 % ¿Quiere Manuel a María y a Pedro?         	-> quierea('Manuel', 'Maria'), quierea('Manuel', 'Pedro').
-% ¿Quiere Manuel a alguien? 			        -> quierea('Manuel', X).
+% ¿Quiere Manuel a alguien? 			        -> quierea('Manuel', 'Alguien').
 % ¿Quiénes son los que quieren a María? 	    -> quierea(X, 'Maria').
 % ¿Quiénes son los que quieren a Pedro? 	    -> quierea(X, 'Pedro').
 % ¿Quiénes son los que se quieren mutuamente? 	-> quierea(X, Y), quierea(Y, X).
-% ¿Quiénes son los que se quieren a sí mismos? 	-> sequiere(X).
-% ¿Se quiere Manuel a sí mismo? 		        -> sequiere('Manuel').
-% ¿Hay alguien que quiera a María? 		        -> alguienquiere('Maria').
-% ¿Hay alguien que quiera a alguien?            -> alguienquiere(A).
-% ¿Hay alguien que se quiera a sí mismo? 	    -> sequiere(X).
-% ¿Hay alguien que quiera a todo el mundo? 	    -> alguienquieretodos(L).
+% ¿Quiénes son los que se quieren a sí mismos? 	-> quierea(X, 'Asimismo').
+% ¿Se quiere Manuel a sí mismo? 		        -> quierea('Manuel','Asimismo').
+% ¿Hay alguien que quiera a María? 		        -> quierea('Alguien','Maria').
+% ¿Hay alguien que quiera a alguien?            -> quierea('Alguien', 'Alguien').
+% ¿Hay alguien que se quiera a sí mismo? 	    -> quierea('Alguien', 'Asimismo').
+% ¿Hay alguien que quiera a todo el mundo? 	    -> quierea('Alguien', 'Todos').
