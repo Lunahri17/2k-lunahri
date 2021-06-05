9d26b03c9642e7fdef936a771a2413934aa7615b
Lunahri17 <jeremy17games@hotmail.com>
2021-06-05 12:33:52 -0300

    Punto 1 gustavo

---
+++ Paradigmas/ProgLogica/TPN2/punto1.pl
@@ -0,0 +1,9 @@
+% --- Punto 1 ---
+% ¿Le gusta algo a Juan?
+gustar('Juan', 'Algo').
+
+% ¿Le gustan a Juan las almendras?
+gustar('Juan', 'Almendras').
+
+% ¿Qué es lo que le gusta a Juan?
+gustar('Juan', X).
