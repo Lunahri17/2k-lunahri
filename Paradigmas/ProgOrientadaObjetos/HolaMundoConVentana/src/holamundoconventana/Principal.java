17caf6ad2fb35019fb8c71d6e3b7564b803299c5
Lunahri17 <lunahri@outlook.com>
2021-10-25 20:56:19 -0300

    Clase 6 Ejemplo holamundo con ventana

---
+++ Paradigmas/ProgOrientadaObjetos/HolaMundoConVentana/src/holamundoconventana/Principal.java
@@ -0,0 +1,12 @@
+package holamundoconventana;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        window ventana = new window();
+        ventana.setVisible(true);
+        windows2 ventana2 = new windows2();
+        ventana2.setVisible(true);
+    }
+    
+}
