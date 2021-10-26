206c305f55f38ac35a9f26c854d1c32c4ba29612
Lunahri17 <lunahri@outlook.com>
2021-10-26 18:34:25 -0300

    polimorfismo

--- Paradigmas/ProgOrientadaObjetos/Clase4/src/clase4/Clase4.java
+++ Paradigmas/ProgOrientadaObjetos/Clase4/src/clase4/Clase4.java
@@ -5,13 +5,27 @@ public class Clase4 {
     public static void main(String[] args) {
         Cuadrado c = new Cuadrado(5, "rojo");
         
-        c.dibujar();
+        //c.dibujar();
         System.out.println();
         System.out.println("--------------------------");
         System.out.println();
         
         Rectangulo r = new Rectangulo(5,12,"azul");
-        r.dibujar();
+        //r.dibujar();
+        
+        
+       //polimorfismo
+       
+       Figura[] figuras = new Figura[3];
+       figuras[0] = c;
+       figuras[1] = r;
+       
+        for (int i = 0; i < 2; i++) {
+            Figura f = figuras[i];
+            f.dibujar();
+            System.out.println("--------------------------");
+        }
+       
     }
     
 }
